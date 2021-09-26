b593956c489bfd13bbe89853a62961a1938f0337
Sarfaraz <khansarfaraz2141@gmail.com>
2021-09-26 12:37:57 +0530

    Logging a User Out

--- server.js
+++ server.js
@@ -47,6 +47,15 @@ myDB(async (client) => {
     res.render(process.cwd() + '/views/pug/profile', { username: req.user.username });
   });
 
+  app.route('/logout').get((req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+
+  app.use((req, res, next) => {
+    res.status(404).type('text').send('Not Found');
+  });
+
   // Serialization and deserialization here...
   passport.serializeUser((user, done) => {
     done(null, user._id);
