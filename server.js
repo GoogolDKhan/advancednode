c3f46b1edf2049c506bfc14442ccbd10950ec598
Sarfaraz <khansarfaraz2141@gmail.com>
2021-09-26 12:36:22 +0530

    Put a Profile Together

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ myDB(async (client) => {
   });
 
   app.route('/profile').get(ensureAuthenticated, (req, res) => {
-    res.render(process.cwd() + '/views/pug/profile');
+    res.render(process.cwd() + '/views/pug/profile', { username: req.user.username });
   });
 
   // Serialization and deserialization here...
