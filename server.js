dac9b273e60729b1ee0e84d3a6e5759d34fca56d
Sarfaraz <khansarfaraz2141@gmail.com>
2021-09-28 00:16:52 +0530

    Announce New Users

--- server.js
+++ server.js
@@ -56,13 +56,20 @@ myDB(async (client) => {
   let currentUsers = 0;
   io.on('connection', (socket) => {
     ++currentUsers;
-    io.emit('user count', currentUsers);
-    console.log('user ' + socket.request.user.username + ' connected');
-
+    io.emit('user', {
+      name: socket.request.user.name,
+      currentUsers,
+      connected: true
+    });
+    console.log('A user has connected');
     socket.on('disconnect', () => {
       console.log('A user has disconnected');
       --currentUsers;
-      io.emit('user count', currentUsers);
+      io.emit('user', {
+        name: socket.request.user.name,
+        currentUsers,
+        connected: false
+      });
     });
   });
 }).catch((e) => {
